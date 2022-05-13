a0f90bf82a73bdcd417fecf5f9301b635077d95b
Peter Li <rightamlim@gmail.com>
2022-05-13 09:43:19 -0700

    Fish part is gone, added flashlight

--- PyterGame.py
+++ PyterGame.py
@@ -5,8 +5,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 SKY_BLUE = (95, 165, 228)
-SAND_YELLOW = (	194, 178, 128)
-WIDTH = 1600
+WIDTH = 1200
 HEIGHT = 1200
 TITLE = "Pyter Game"
 
@@ -15,51 +14,19 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
 
         # Image
-        self.image = pygame.image.load("./assets/player_fish_left.png")
-        self.image = pygame.transform.scale(self.image, (79, 51))
-
-        self.change_x = 0
-        self.change_y = 0
+        self.image = pygame.image.load("./assets/box.jpg")
+        self.image = pygame.transform.scale(self.image, (15, 15))
 
         self.rect = self.image.get_rect()
 
     def update(self):
         """ Move the player. """
-        self.rect.x += self.change_x
+        self.rect.center = pygame.mouse.get_pos()
 
-        self.rect.y += self.change_y
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
 
-        if self.change_x < 0:
-            self.image = pygame.image.load("./assets/player_fish_left.png")
-            self.image = pygame.transform.scale(self.image, (79, 51))
-
-        if self.change_x > 0:
-            self.image = pygame.image.load("./assets/player_fish_right.png")
-            self.image = pygame.transform.scale(self.image, (79, 51))
-
-    def go_left(self):
-        """ Called when the user hits the left arrow. """
-        self.change_x = -6
-
-    def go_right(self):
-        """ Called when the user hits the right arrow. """
-        self.change_x = 6
-
-    def go_up(self):
-        """ Called when the user hits the left arrow. """
-        self.change_y = -6
-
-    def go_down(self):
-        """ Called when the user hits the right arrow. """
-        self.change_y = 6
-
-    def stop_x(self):
-        """ Called when the user lets off the keyboard. """
-        self.change_x = 0
-
-    def stop_y(self):
-        """ Called when the user lets off the keyboard. """
-        self.change_y = 0
 
 def main():
     pygame.init()
@@ -68,6 +35,10 @@ def main():
     size = (WIDTH, HEIGHT)
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption(TITLE)
+    pygame.mouse.set_visible(False)
+
+    background = pygame.image.load("./assets/floor_tiles.png").convert()
+
 
     # ----- LOCAL VARIABLES
     done = False
@@ -78,39 +49,47 @@ def main():
     player = Player()
     all_sprites_group.add(player)
 
+    radius = 75
+
+    cover_surf = pygame.Surface((radius*2, radius*2))
+    cover_surf.fill(0)
+    cover_surf.set_colorkey((255, 255, 255))
+    pygame.draw.circle(cover_surf, (255, 255, 255), (radius, radius), radius)
+
+    clicked_sprites = []
+
     # ----- MAIN LOOP
+
     while not done:
         # -- Event Handler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.go_left()
-                if event.key == pygame.K_RIGHT:
-                    player.go_right()
-                if event.key == pygame.K_UP:
-                    player.go_up()
-                if event.key == pygame.K_DOWN:
-                    player.go_down()
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT and player.change_x < 0:
-                    player.stop_x()
-                if event.key == pygame.K_RIGHT and player.change_x > 0:
-                    player.stop_x()
-                if event.key == pygame.K_UP and player.change_y < 0:
-                    player.stop_y()
-                if event.key == pygame.K_DOWN and player.change_y > 0:
-                    player.stop_y()
+                # get a list of all sprites that are under the mouse cursor
+                clicked_sprites = [s for s in all_sprites_group if s.rect.collidepoint(pos)]
+                print(clicked_sprites)
+                # do something with the clicked sprites...
+
+        clip_center = pygame.mouse.get_pos()
+
+        clicked_sprites.clear()
+
+        print(clicked_sprites)
+
+        screen.fill(0)
+        clip_rect = pygame.Rect(clip_center[0] - radius, clip_center[1] - radius, radius * 2, radius * 2)
+        screen.set_clip(clip_rect)
 
         # ----- LOGIC
         all_sprites_group.update()
 
         # ----- RENDER
-        screen.fill(SKY_BLUE)
-        pygame.draw.rect(screen, SAND_YELLOW, pygame.Rect(0, HEIGHT - 150, WIDTH, 150))
+        screen.blit(background, (0, 0))
+        screen.blit(cover_surf, clip_rect)
         all_sprites_group.draw(screen)
 
         # ----- UPDATE DISPLAY
