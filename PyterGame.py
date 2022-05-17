6dd9436ccb9ad6e2256c8d1c0e5508dae5f8081a
Peter Li <rightamlim@gmail.com>
2022-05-17 12:49:52 -0700

    Changed render order to make sprite render under flashlight and made collision masks for accurate detection

--- PyterGame.py
+++ PyterGame.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 # ----- CONSTANTS
 BLACK = (0, 0, 0)
@@ -10,19 +11,23 @@ WIDTH = 1200
 HEIGHT = 1200
 TITLE = "Pyter Game"
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
         # Image
-        self.image = pygame.image.load("./assets/box.jpg")
-        self.image = pygame.transform.scale(self.image, (15, 15))
+        self.image = pygame.image.load("assets/circle.png")
+        self.image = pygame.transform.scale(self.image, (150, 150))
 
         self.rect = self.image.get_rect()
+
+        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
         """ Move the player. """
         self.rect.center = pygame.mouse.get_pos()
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -32,7 +37,14 @@ class Enemy(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (96, 70))
 
         self.rect = self.image.get_rect()
+        self.rect.center = random_coords()
 
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+def random_coords() -> tuple:
+    """Returns a random x, y coordinate between 0 to WIDTH and 0 to HEIGHT"""
+    return random.randrange(40, WIDTH - 40), random.randrange(40, HEIGHT - 40)
 
 
 def main():
@@ -46,7 +58,6 @@ def main():
 
     background = pygame.image.load("./assets/floor_tiles.png").convert()
 
-
     # ----- LOCAL VARIABLES
     # Pygame conditions
     done = False
@@ -54,28 +65,37 @@ def main():
 
     # Sprites
     all_sprites_group = pygame.sprite.Group()
+    player_sprites_group = pygame.sprite.Group()
     enemy_sprites_group = pygame.sprite.Group()
 
     player = Player()
+    player_sprites_group.add(player)
     all_sprites_group.add(player)
 
     enemy = Enemy()
-    all_sprites_group.add(enemy)
     enemy_sprites_group.add(enemy)
-
-    enemy.rect.x = 100
-    enemy.rect.y = 100
+    all_sprites_group.add(enemy)
 
     # Light circle
     radius = 75
 
-    cover_surf = pygame.Surface((radius*2, radius*2))
+    cover_surf = pygame.Surface((radius * 2, radius * 2))
     cover_surf.fill(0)
     cover_surf.set_colorkey((255, 255, 255))
     pygame.draw.circle(cover_surf, (255, 255, 255), (radius, radius), radius)
 
     # List of clicked sprites
     clicked_sprites = []
+
+    # Time
+    time_to_find = 15000
+    time_to_catch = 10000
+
+    time_spawned = 0
+    time_found = 0
+    time_caught = 0
+
+    # Sounds
 
     # ----- MAIN LOOP
 
@@ -89,36 +109,41 @@ def main():
                 pos = pygame.mouse.get_pos()
 
                 # get a list of all sprites that are under the mouse cursor
-                clicked_sprites = [s for s in all_sprites_group if s.rect.collidepoint(pos)]
+                clicked_sprites = [s for s in enemy_sprites_group if s.rect.collidepoint(pos)]
                 print(clicked_sprites)
                 # do something with the clicked sprites...
 
         clicked_sprites.clear()
 
-        print(clicked_sprites)
-
         # Light circle
         clip_center = pygame.mouse.get_pos()
-        print(clip_center)
 
         screen.fill(0)
         clip_rect = pygame.Rect(clip_center[0] - radius, clip_center[1] - radius, radius * 2, radius * 2)
         screen.set_clip(clip_rect)
 
         # ----- LOGIC
-        all_sprites_group.update()
+        player_sprites_group.update()
+
+        enemy_collide = pygame.sprite.spritecollide(player, enemy_sprites_group, False, pygame.sprite.collide_mask)
+
+        if len(enemy_collide) > 0:
+
+            time_found = pygame.time.get_ticks()
 
         # ----- RENDER
         screen.blit(background, (0, 0))
-        screen.blit(cover_surf, clip_rect)
 
         # Render enemy only if they are close enough to the light
-        if math.sqrt((enemy.rect.x + (enemy.rect.width / 2) - clip_center[0]) ** 2 + (enemy.rect.y + (enemy.rect.height / 2) - clip_center[1]) ** 2) <= 90:
+        if math.sqrt((enemy.rect.x + (enemy.rect.width / 2) - clip_center[0]) ** 2 + (enemy.rect.y + (enemy.rect.height / 2) - clip_center[1]) ** 2) <= 120:
             enemy_sprites_group.draw(screen)
+
+        screen.blit(cover_surf, clip_rect)
 
         # ----- UPDATE DISPLAY
         pygame.display.flip()
         clock.tick(75)
+
 
     pygame.quit()
 
